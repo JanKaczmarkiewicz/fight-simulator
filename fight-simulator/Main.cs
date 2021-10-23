f0db4992ad1fc562a6d6264c840c34986f42f861
Jan KACZMARKIEWICZ <jan.kaczmarkiewicz@lastminute.com>
2021-10-23 10:28:26 +0200

    feat: draw circles on canvas

---
+++ fight-simulator/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace fight_simulator
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
